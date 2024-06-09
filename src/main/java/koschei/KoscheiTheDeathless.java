a30aa8b2a4e5a8dd2c7a035265da0acb958b86ce
Vladislav Zhurnalev <vkadzhu@gmail.com>
2024-06-10 01:38:57 +0300

    First commit

---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,25 @@
+package koschei;
+
+import koschei.models.Ocean1;
+import koschei.models.Rabbit4;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+    private Rabbit4 rabbit4;
+
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
+    }
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+
+
+}
